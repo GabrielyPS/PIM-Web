6ee2cc081d722080cb472461f36ab3559a841540
Gabriely Pelissen <gaby.siq@hotmail.com>
2021-11-25 23:04:11 -0300

    Tela acomodacoes

---
+++ Controllers/AcomodacoesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIM_Web.Controllers
+{
+    public class AcomodacoesController : Controller
+    {
+        // GET: Acomodacoes
+        public ActionResult Acomodacoes_Index()
+        {
+            return View();
+        }
+    }
+}
