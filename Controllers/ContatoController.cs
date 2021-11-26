94d3f75b8f1bdfd5f27cbe84544515ee6a4a0924
Gabriely Pelissen <gaby.siq@hotmail.com>
2021-11-26 19:32:40 -0300

    teste

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace PIM_Web.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: Acomodacoes
+        public ActionResult Contato_Index()
+        {
+            return View();
+        }
+    }
+}
